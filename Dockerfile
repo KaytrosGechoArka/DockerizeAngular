111c349203c7ac62cf9a9929203d81ced8a34633
kaytr <kaytrosgech@gmail.com>
2023-10-06 21:02:01 +0300

    Minor Commit

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ RUN npm install -g @angular/cli
 RUN npm install
 RUN ng build 
 
-FROM nginx:1.17.1-alpine
+FROM nginx:latest
 COPY --from=build /app/dist/employeemanagerapp /usr/share/nginx/html
 COPY src/nginx/etc/conf.d/nginx.conf /etc/nginx/nginx.conf
 EXPOSE 80
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,111 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee'
+import { HttpErrorResponse } from '@angular/common/http'
+import { EmployeeService } from './employee.service'
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employeemanagerapp';
+export class AppComponent implements OnInit {
+
+  public employees!: Employee[];
+  public editEmployee?: Employee | null;
+  public deleteEmployee?: Employee | null;
+
+  constructor(private employeeService:EmployeeService){}
+  
+  ngOnInit(){
+    this.getEmployees();
+  }
+
+  public getEmployees():void{
+    this.employeeService?.getEmployees().subscribe(
+      (response: Employee[])=>{
+        this.employees=response;
+      },
+      (error:HttpErrorResponse)=>{
+        alert(error.message);
+      }
+    );
+  }
+
+  public onAddEmployee(addForm:NgForm){
+    document.getElementById('add-employee-form')?.click();
+    this.employeeService.addEmployees(addForm.value).subscribe(
+      (response:Employee)=>{
+        console.log(response);
+        this.getEmployees();
+        addForm.reset()
+      },
+      (error:HttpErrorResponse)=>{
+        alert(error.message)
+      }
+    );
+  }
+
+  public onUpdateEmployee(employee: Employee) {
+    document.getElementById('add-employee-form')?.click();
+    this.employeeService.updateEmployees(employee).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+      }
+    );
+  }
+  public onDeleteEmployee(employeeId: number) :void{
+    document.getElementById('add-employee-form')?.click();
+    this.employeeService.deleteEmployees(employeeId).subscribe(
+      (response: void) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+      }
+    );
+  }
+
+  public searchEmployees(key: string): void {
+    console.log(key);
+    const results: Employee[] = [];
+    for (const employee of this.employees) {
+      if (employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || employee.phone.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || employee.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
+        results.push(employee);
+      }
+    }
+    this.employees = results;
+    if (results.length === 0 || !key) {
+      this.getEmployees();
+    }
+  }
+
+  public onOpenModal(employee: Employee  | null=null, mode: string ): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+       button.setAttribute('data-target', '#addEmployeeModal');
+    }
+    if (mode === 'edit') {
+      this.editEmployee = employee  ;
+       button.setAttribute('data-target', '#updateEmployeeModal');
+     }
+     if (mode === 'delete') {
+      this.deleteEmployee = employee;
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+    container?.appendChild(button);
+    button.click();
+  }
 }
